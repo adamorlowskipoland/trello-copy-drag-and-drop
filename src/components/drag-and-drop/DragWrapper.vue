a20794be3f489ae8a6c1625a2b53e863781318ec
Adam Orłowski <adamorlowskipoland@icloud.com>
2022-11-01 11:45:51 +0100

    lint fix

--- src/components/drag-and-drop/DragWrapper.vue
+++ src/components/drag-and-drop/DragWrapper.vue
@@ -13,7 +13,7 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
-import type { TransferData } from '@/composables/board';
+import type { TransferData } from "@/composables/board";
 
 const props = defineProps<{
   transferData: TransferData;
