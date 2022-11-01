e839e699fe4ffe62df409cb6fe2dc6ec5a1e5cbf
Adam Orłowski <adamorlowskipoland@icloud.com>
2022-11-01 11:41:11 +0100

    delete watcher

--- src/composables/dragAndDrop.ts
+++ src/composables/dragAndDrop.ts
@@ -1,11 +1,8 @@
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { TransferData } from "@/composables/board";
 
 const draggedItem = ref<TransferData | null>(null);
-watch(draggedItem, (newVal, oldValue) => {
-  console.log("%c Line: old, msg: : ", "color: yellow", oldValue);
-  console.log("%c Line: new, msg: : ", "color: lightseagreen", newVal);
-});
+
 export const useDragAndDrop = () => {
   return {
     draggedItem,
