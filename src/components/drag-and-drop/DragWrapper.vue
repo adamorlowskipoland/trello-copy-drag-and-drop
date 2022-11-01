6f352b2422fa408b6aba594de1fffd21860af999
Adam Orłowski <adamorlowskipoland@icloud.com>
2022-11-01 11:39:38 +0100

    add type to prop transferData

--- src/components/drag-and-drop/DragWrapper.vue
+++ src/components/drag-and-drop/DragWrapper.vue
@@ -13,9 +13,10 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
+import type { TransferData } from '@/composables/board';
 
 const props = defineProps<{
-  transferData: unknown;
+  transferData: TransferData;
 }>();
 
 const isDragging = ref(false);
